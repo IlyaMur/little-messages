84a94fd3a768af22d1ad2e41fb711beef28baa5c
Ilya Mur <ilyamur@hotmail.com>
2021-12-14 12:32:39 +0300

    redirect to login if no auth

---
+++ src/App/Controllers/Items.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ilyamur\PhpMvc\App\Controllers;
+
+use Ilyamur\PhpMvc\App\Auth;
+use Ilyamur\PhpMvc\Core\View;
+
+class Items extends \Ilyamur\PhpMvc\Core\Controller
+{
+    public function indexAction()
+    {
+        if (!Auth::isLoggedIn()) {
+            $this->redirect('/login');
+        }
+
+        View::renderTemplate('Items/index');
+    }
+}
