ea91b2ac49d39e72e2388ecef54d7605f3fae636
Ilya Mur <ilyamur@hotmail.com>
2022-01-18 14:59:19 +0300

    prepare to deploy

--- Makefile
+++ Makefile
@@ -1,11 +1,8 @@
 install:
 	composer install
-	
 test:
 	composer exec phpunit tests
-
-test-coverage:
-	composer exec --verbose phpunit -- --testsuite gh-actions --coverage-clover build/logs/clover.xml
-	
 lint:
 	composer exec phpcs -- --standard=PSR12 src
+gh-test:
+	composer exec --verbose phpunit -- --testsuite gh-actions
--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -1,16 +1,16 @@
 <?php
 
-// declare(strict_types=1);
+declare(strict_types=1);
 
-// namespace Ilyamur\PhpMvc\Tests\Unit\Models;
+namespace Ilyamur\PhpMvc\Tests\Unit\Models;
 
-// use PHPUnit\Framework\TestCase;
-// use Ilyamur\PhpMvc\Tests\Unit\Models\TestDoubles\BaseModelChild;
+use PHPUnit\Framework\TestCase;
+use Ilyamur\PhpMvc\Tests\Unit\Models\TestDoubles\BaseModelChild;
 
-// class BaseModelTest extends TestCase
-// {
-//     public function testReturnsPDOObject()
-//     {
-//         $this->assertInstanceOf('PDO', BaseModelChild::getDB());
-//     }
-// }
+class BaseModelTest extends TestCase
+{
+    public function testReturnsPDOObject()
+    {
+        $this->assertInstanceOf('PDO', BaseModelChild::getDB());
+    }
+}
